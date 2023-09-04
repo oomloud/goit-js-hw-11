d0a02679fddebac962930637b2c0e49958a27dad
oomloud <47818790+oomloud@users.noreply.github.com>
2023-09-04 21:18:45 +0000

    Deploying to gh-pages from @ oomloud/goit-js-hw-11@f8fd6bca2c423e652afe39d6ffd7028e47a7525a 🚀

---
+++ index.bdee43f2.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".search-form"),input:document.querySelector(".input-search"),gallery:document.querySelector(".gallery")}).form.addEventListener("submit",(async function(e){e.preventDefault()}));
+//# sourceMappingURL=index.bdee43f2.js.map
