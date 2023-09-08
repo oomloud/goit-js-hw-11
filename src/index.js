1612ea66b2bcfebce5c19a30b5434b97ac0a713a
Serhiy Chepa <schepa@pixelfederation.com>
2023-09-09 00:30:04 +0200

    basic functionality done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // TODO: BONUS: add switcher between page loading and infinite scroll
 // Створи фронтенд частину застосунку пошуку і перегляду зображень за ключовим словом. Додай оформлення елементів інтерфейсу.
-// В початковому стані кнопка повинна бути прихована.
-// Після першого запиту кнопка з'являється в інтерфейсі під галереєю.
 // При повторному сабміті форми кнопка спочатку ховається, а після запиту знову відображається.
 // У відповіді бекенд повертає властивість totalHits - загальна кількість зображень, які відповідають критерію пошуку (для безкоштовного акаунту). Якщо користувач дійшов до кінця колекції, ховай кнопку і виводь повідомлення з текстом "We're sorry, but you've reached the end of search results.".
 
@@ -11,35 +9,57 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const elements = {
   form: document.querySelector(".search-form"),
   input: document.querySelector(".input-search"),
-  gallery: document.querySelector(".gallery")
+  gallery: document.querySelector(".gallery"),
+  loadMore: document.querySelector(".js-load-more")
 }
 
 elements.form.addEventListener('submit', submitHandler);
+elements.loadMore.addEventListener('click', loadMoreHandler);
 
 const axios = require('axios');
 const API_KEY = '1482357-ebc3f86fcb5bdf18b64ce1456';
+const imgPerPage = 40;
 let page = 1;
 
-const params = {
-  method: 'get',
-  baseURL: 'https://pixabay.com/api/',
-}
-
 async function submitHandler(evt) {
-  evt.preventDefault();
-  const { searchQuery } = evt.currentTarget.elements;
-  getImages(searchQuery.value)
-    .then((data) => {
-      elements.gallery.innerHTML = '';
-      elements.gallery.insertAdjacentHTML("afterbegin", createMarkup(data))
-      const blob = document.querySelector(".gallery-item-image");
+  try {
+    evt.preventDefault();
+    const { searchQuery } = evt.currentTarget.elements;
+    const images = await getImages(searchQuery.value);
+    elements.gallery.innerHTML = '';
+    elements.gallery.insertAdjacentHTML("afterbegin", createMarkup(images));
+    if (page * imgPerPage < images.totalHits) {
+      elements.loadMore.classList.replace('load-more-hidden', 'load-more');
+    }
+  }
+  catch {
+    elements.loadMore.classList.replace('load-more', 'load-more-hidden');
+  }
 
-      // elements.gallery.children.map(image => image)
-    })
-    .catch()
+
 }
 
+
+async function loadMoreHandler(evt) {
+
+  try {
+    page += 1;
+    const keyword = elements.input.value;
+    const images = await getImages(keyword);
+    elements.gallery.insertAdjacentHTML("beforeend", createMarkup(images));
+    if (page * imgPerPage >= images.totalHits) {
+
+      elements.loadMore.classList.replace('load-more', 'load-more-hidden');
+    }
+  }
+  catch {
+    elements.loadMore.classList.replace('load-more', 'load-more-hidden');
+  }
+}
+
+// getting response from server based on keyword
 async function getImages(keyword) {
+  console.log(page);
   const resp = await axios.get('https://pixabay.com/api/', {
     params: {
       key: API_KEY,
@@ -47,23 +67,23 @@ async function getImages(keyword) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 40,
+      per_page: imgPerPage,
       page: page
     },
     responseType: 'json',
     headers: {
       "Accept": "application/json",
-      // "Accept-Encoding": "deflate"
     }
   });
+  console.log(resp);
   if (resp.status === '200') {
     throw new Error(resp.statusText);
   }
-  return resp.data.hits;
+  return resp.data;
 }
 
 function createMarkup(arr) {
-  let galleryItem = arr
+  let galleryItem = arr.hits
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
       `<div class="gallery-item">
         <img class="gallery-item-image" src="${webformatURL}" alt="${tags}">      
